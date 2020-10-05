df8bc6dfd8b38327ffd67ca7798bca046ba2a7ae
Saranraj A <58389152+saranraj18@users.noreply.github.com>
2020-10-05 11:41:58 +0530

    New

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:hackinutu/styles/color.dart';
 
 void main() {
   runApp(Police());
@@ -8,12 +7,6 @@ void main() {
 class Police extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: dBlue,
-          ),
-          body: Container(color: lBlue)),
-    );
+    return MaterialApp();
   }
 }
